bbfdcfa900c18b8c0536aaa5724b1f418ff0bef5
Gromych <gromych@yandex-team.ru>
2015-02-16 00:26:52 +0300

    gets title and preview from article text

--- helpers/previews/index.js
+++ helpers/previews/index.js
@@ -2,6 +2,7 @@ var through = require('through2');
 var meta = require('../meta');
 var _ = require('lodash');
 var path = require('path');
+var marked = require('marked');
 
 module.exports = function(previews) {
 	return through.obj(function(file, enc, callback) {
@@ -15,21 +16,53 @@ module.exports = function(previews) {
 			return;
 		}
 
+
 		var source = file.contents.toString();
 		var articleMeta = meta.extract(source);
 
 		var preview = _.pick(articleMeta, [
-			'title',
 			'date',
-			'preview',
 			'more'
 		]);
 
 		var filePath = path.dirname(file.path);
 		preview.url = filePath.replace(file.cwd + '/src/content', '');
 
+		var tokens = marked.lexer(source);
+		preview.preview = getPreview(tokens);
+		preview.title = getTitle(tokens);
+
 		previews.push(preview);
 
 		callback(null, file);
 	});
 };
+
+function getPreview(tokens) {
+	var limit = 5;
+	var count = 0;
+	var previewTokens = [];
+	previewTokens.links = {};
+
+	for (var i = 0, max = tokens.length; i < max; i++) {
+		if (count == limit || (count && tokens[i].type == 'heading')) {
+			break;
+		}
+
+		if (tokens[i].type == 'paragraph') {
+			previewTokens.push(tokens[i]);
+			count++;
+		}
+	}
+
+
+	return marked.parser(previewTokens);
+}
+
+function getTitle(tokens) {
+	for (var i = 0, max = tokens.length; i < max; i++) {
+		if (tokens[i].type == 'heading') {
+			return tokens[i].text;
+		}
+	}
+}
