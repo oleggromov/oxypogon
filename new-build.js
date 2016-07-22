af51a43590923afc3d08a14755b0c2cff1536ec6
Gromych <gromych@yandex-team.ru>
2016-07-22 21:30:20 +0300

    Changes config and its passing mechanism

--- new-build.js
+++ new-build.js
@@ -1,4 +1,6 @@
 var resolve = require('path').resolve;
 var Colibri = require('./lib/classes/Colibri');
 
-new Colibri(resolve('./config'));
+var config = require('./config');
+
+new Colibri(config);
